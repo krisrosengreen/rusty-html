bc4b7eb38a75efcb4f2bd734d82b1bbd4e0613b9
Kristoffer <rosengreenpedersen@mail.com>
2022-10-30 09:31:43 +0100

    Yikes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // Get rid of warnings derived from macros
 #![allow(unused_mut)]
 #![allow(dead_code)]
+#![allow(missing_fragment_specifier)]
 
 #[derive(Clone, Debug)]
 struct AttrPair {
@@ -26,17 +27,6 @@ macro_rules! html_node {
             $(attr.push(AttrPair{attr_name: String::from(stringify!($attr_name)), attr_val: String::from($attr_val) });)*
 
             let node_name = String::from(stringify!($item_name));
-
-            Node {node_name: node_name, attrs: attr.clone()}
-        }
-    };
-    (/ $item_name:ident $($attr_name:ident=$attr_val:literal )*) => {
-        {
-            let mut attr: Vec<AttrPair> = Vec::new();
-            $(attr.push(AttrPair{attr_name: String::from(stringify!($attr_name)), attr_val: String::from($attr_val) });)*
-
-            let node_name = String::from(stringify!($item_name));
-
             Node {node_name: node_name, attrs: attr.clone()}
         }
     };
@@ -46,7 +36,6 @@ macro_rules! html {
     ($(<$(/)?$tag:ident $($attr_name:ident=$attr_val:literal )*>)*) => {
         {
             let mut nodes: Vec<Node> = Vec::new();
-
             $(nodes.push(html_node!($tag $($attr_name=$attr_val )*));)*
 
             HTMLCode{nodes: nodes}
@@ -55,6 +44,7 @@ macro_rules! html {
 }
 
 fn main() {
+    // An example
     let code1: HTMLCode = html!(
 
         <html>
@@ -74,12 +64,11 @@ fn main() {
     }
 
     // Another example
-
     let code2: HTMLCode = html!(
 
         <help type="Common bug">
             <link src="http://www.helphere.com"></link>
-        </html>
+        </help>
 
     );
 
